ce120d0545f7ce86f1c4ddf492b6416ca96104d6
Nicklas Utgaard <nicklas.utgaard@eika.no>
2019-02-22 11:44:14 +0100

    Updates

---
+++ precache-manifest.222898299ef5bc36f21b0099ee301c4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "024fd9e4ff2265f8dc9e",
+    "url": "/domain-codegen/static/js/runtime~main.024fd9e4.js"
+  },
+  {
+    "revision": "2a478defe29aa9efb0ac",
+    "url": "/domain-codegen/static/js/main.2a478def.chunk.js"
+  },
+  {
+    "revision": "f9a48c13396205e1db77",
+    "url": "/domain-codegen/static/js/1.f9a48c13.chunk.js"
+  },
+  {
+    "revision": "2a478defe29aa9efb0ac",
+    "url": "/domain-codegen/static/css/main.03334da0.chunk.css"
+  },
+  {
+    "revision": "693e1dd1dc62ea645eb983ec2d79b131",
+    "url": "/domain-codegen/index.html"
+  }
+];

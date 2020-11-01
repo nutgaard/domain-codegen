6f35423fd5b71d67e9f161fb3e6687743fce4056
Nicklas Utgaard <nutgaard@gmail.com>
2020-11-01 18:11:15 +0100

    Updates

---
+++ precache-manifest.ab61403414fc23b368ab742b582fabb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fd132407e2c94f0c114",
+    "url": "/domain-codegen/static/css/main.ab789f7c.chunk.css"
+  },
+  {
+    "revision": "5fd132407e2c94f0c114",
+    "url": "/domain-codegen/static/js/main.5fd13240.chunk.js"
+  },
+  {
+    "revision": "92b2d496672f6e0abd1d",
+    "url": "/domain-codegen/static/js/1.92b2d496.chunk.js"
+  },
+  {
+    "revision": "024fd9e4ff2265f8dc9e",
+    "url": "/domain-codegen/static/js/runtime~main.024fd9e4.js"
+  },
+  {
+    "revision": "351f11f0fc584d8a5c96287e2825da0d",
+    "url": "/domain-codegen/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/domain-codegen/precache-manifest.e7718a286aa1596f3b610484b71f6b62.js"
+  "/domain-codegen/precache-manifest.ab61403414fc23b368ab742b582fabb2.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/domain-codegen/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
